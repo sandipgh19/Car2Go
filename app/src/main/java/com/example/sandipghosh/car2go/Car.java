93f17d2b2df528596eaf58c1c06571f6b607c677
sandipghosh <sandipgh19@gmail.com>
2017-09-27 21:57:43 +0530

    Starting the view of app

---
+++ app/src/main/java/com/example/sandipghosh/car2go/Car.java
@@ -0,0 +1,45 @@
+package com.example.sandipghosh.car2go;
+
+/**
+ * Created by sandipghosh on 27/09/17.
+ */
+
+public class Car {
+
+    private String address;
+    private String coordinates;
+    private String name;
+    private String interior;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInterior() {
+        return interior;
+    }
+
+    public void setInterior(String interior) {
+        this.interior = interior;
+    }
+}
